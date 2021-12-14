a74e4619696eea6a38b5f0bab79b5bc56f2e0b80
kushkurl <kushagra.kurl@gmail.com>
2021-12-14 08:49:51 +0530

    addiing form processing app

---
+++ MyToDoList/Models/RegisterDetails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MyToDoList.Models
+{
+    public class RegisterDetails
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Address { get; set; }
+
+        [Required]
+        [Display(Name = "Date of Birth")]
+        public DateTime? DoB { get; set; }      
+        
+        [Required]
+        [CreditCard]
+        public string CardNumber { get; set; }
+    }
+}
---
+++ MyToDoList/Pages/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace MyToDoList.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        [FromForm]
+        public Models.RegisterDetails AddTask { get; set; }
+
+
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+
+            _logger = logger;
+        }
+
+        public void OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return;
+            }
+            else
+            { 
+                try
+                {
+                    if(AddTask.DoB >= DateTime.Now){
+                        
+                        return;
+                    }
+
+                    Redirect("~/Welcome");
+                }
+                catch (Exception e) { Console.WriteLine(e.Message); }
+            }
+        }
+    }
+}
